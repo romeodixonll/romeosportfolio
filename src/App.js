e369310e7eb34552282f2f5c2045da356ae16f88
Romeo Dixon <romeodixon2nd@gmail.com>
2024-11-04 17:41:19 -0600

    made changes and add photos

--- src/App.js
+++ src/App.js
@@ -20,14 +20,14 @@ const App = () => {
         setResumeData(data);
       });
   }, []);
-
+  
   return (
     <div className="App">
       <Header data={resumeData.main} />
       <About data={resumeData.main} />
       <Resume data={resumeData.resume} />
       <Portfolio data={resumeData.portfolio} />
-      <Testimonials data={resumeData.testimonials} />
+      {/* <Testimonials data={resumeData.testimonials} /> */}
       <Contact data={resumeData.main} />
       <Footer data={resumeData.main} />
     </div>
